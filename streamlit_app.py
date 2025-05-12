6e415a9893239e5d40f8c659015115e01ec3a717
korshun1983 <33003712+korshun1983@users.noreply.github.com>
2025-05-12 08:09:16 +0000

    First tests of app designer

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+t = np.arange(0, 100, 0.1, dtype = float)
+y = np.sin(t)
+
+chart_data = pd.DataFrame(
+    {
+        "col1": t,
+        "col2": y
+    }
+)
+
+st.line_chart(
+    chart_data,
+    x="col1",
+    y="col2",
+    x_label="t",
+    y_label="y"
 )
