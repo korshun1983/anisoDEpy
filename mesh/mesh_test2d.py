3cc661ebb1f8edca99baef7dac4ab698724a74cf
korshun1983 <av_kozlov@inbox.ru>
2025-09-01 15:30:31 +0300

    triangular 2d mesh test with added points

--- mesh/mesh_test2d.py
+++ mesh/mesh_test2d.py
@@ -18,7 +18,8 @@ for simplex in tri.simplices:
 
 # Convert new_points to a NumPy array and combine with original points
 new_points = np.array(new_points)
-all_points = np.vstack((points, new_points))
+all_points = points
+#all_points = np.vstack((points, new_points))
 
 # Re-triangulate with the expanded set of points
 tri_refined = Delaunay(all_points)
@@ -27,6 +28,7 @@ tri_refined = Delaunay(all_points)
 plt.figure(figsize=(8, 6))
 plt.triplot(all_points[:, 0], all_points[:, 1], tri_refined.simplices, color='blue', alpha=0.7)
 plt.plot(all_points[:, 0], all_points[:, 1], 'o', markersize=4, color='red') # Plot all points
+plt.plot(new_points[:, 0], new_points[:, 1], 'o', markersize=4, color='red') # Plot new points
 plt.title('2D Triangular Mesh with In-Triangle Points')
 plt.xlabel('X-coordinate')
 plt.ylabel('Y-coordinate')
